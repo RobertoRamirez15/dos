30643e9ceb120c257425e58ddb87307f003cd57f
RobertoRamirez15 <damcb164e29@gmail.com>
2015-12-05 10:53:18 -0600

    diecinueve

--- js/acciones.js
+++ js/acciones.js
@@ -15,17 +15,16 @@ document.addEventListener("deviceready",function(){
 			  var datosCalzado = JSON.parse(resultado);
 			  alert (datosCalzado.tenis[0].modelo);
 		    $('#contenido_calzado').empty();
-					for(var $x = 0; $x < datosCalzado.tenis.length; $x++)
-						{
-				
-			$('#contenido_calzado').append('<div><div width="318" height="189"><img src="http://192.168.1.178/icascatalogo/recursos/imagenes/fotos/ '+ datosCalzado.tenis[x].modelo +' .jpg"  align="left"/><div align="right">modelo '+ datosCalzado.tenis[x].modelo +' </div><div align="center"> <p> '+ datosCalzado.tenis[x].nombre_tenis +' </p> <p> '+ datosCalzado.tenis[x].tipo +' </p> <p>talla(s) disponibles '+ datosCalzado.tenis[x].longitud_disponible +' </p> <p>color(s) disponibles '+ datosCalzado.tenis[x].color_disponible +' </p><p> '+ datosCalzado.tenis[x].marca +' </p><div align="right"> <p> $ '+ datosCalzado.tenis[x].precio +' </p></div></div></div></div>');
-			}
-					
 			$(':mobile-pagecontainer').pagecontainer('change', '#todo',{
 				transition: 'pop'
 			});
+			for(var $x = 0; $x < datosCalzado.tenis.length; $x++)
+			
+			{
+				
+			$('#contenido_calzado').append('<div><div width="318" height="189"><img src="http://192.168.1.178/icascatalogo/recursos/imagenes/fotos/ '+ datosCalzado.tenis[x].modelo +' .jpg"  align="left"/><div align="right">modelo '+ datosCalzado.tenis[x].modelo +' </div><div align="center"> <p> '+ datosCalzado.tenis[x].nombre_tenis +' </p> <p> '+ datosCalzado.tenis[x].tipo +' </p> <p>talla(s) disponibles '+ datosCalzado.tenis[x].longitud_disponible +' </p> <p>color(s) disponibles '+ datosCalzado.tenis[x].color_disponible +' </p><p> '+ datosCalzado.tenis[x].marca +' </p><div align="right"> <p> $ '+ datosCalzado.tenis[x].precio +' </p></div></div></div></div>');
+			}
 		  }
-		
 	  });
 	});
 }); 
