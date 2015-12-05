9e618be84d32e6bac2c2d77f447476afc1d46d42
RobertoRamirez15 <damcb164e29@gmail.com>
2015-12-05 09:23:50 -0600

    once

--- js/acciones.js
+++ js/acciones.js
@@ -2,8 +2,6 @@
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 	$('#todo').on('tap', function(){
-		alert ("dentro de todo");
-
 		
 	  $.ajax({
 		  type: "POST",
@@ -17,9 +15,8 @@ document.addEventListener("deviceready",function(){
 			  var datosCalzado = JSON.parse(resultado);
 		    $('#contenido_calzado').empty();
 			$(':mobile-pagecontainer').pagecontainer('change', '#todo',{
-				trasition: 'pop'
+				transition: 'pop'
 			});
-			
 			for(var $x = 0; $x < datosCalzado.tenis.length; $x++)
 			{
 			$('#contenido_calzado').append('<div><div width="318" height="189"><img src="http://192.168.1.74/icascatalogo/recursos/imagenes/fotos/'+ datosCalzado.tenis[x].modelo +'.jpg"  align="left"/><div align="right">modelo'+ datosCalzado.tenis[x].modelo +'</div><div align="center"> <p>'+ datosCalzado.tenis[x].nombre_tenis +'</p> <p>'+ datosCalzado.tenis[x].tipo +'</p> <p>talla(s) disponibles '+ datosCalzado.tenis[x].longitud_disponible +'</p> <p>color(s) disponibles'+ datosCalzado.tenis[x].color_disponible +' </p><p>'+ datosCalzado.tenis[x].marca +'</p><div align="right"> <p> $'+ datosCalzado.tenis[x].precio +' </p></div></div></div></div>');
